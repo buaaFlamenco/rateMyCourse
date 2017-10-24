4dc70ba23083593bebe52b8d523ae1c8eeb0536c
Hesitater <yizimu@gmail.com>
2017-10-24 18:55:20 +0800

    Interface detailed

--- rateMyCourse/Interface.py
+++ rateMyCourse/Interface.py
@@ -1,134 +1,138 @@
+'''
+Get CourseInfo list from designated school, or from designated department
+'''
+def getCourseList(str school = None, str department = None):
+    pass
+
+'''
+Get CourseInfo via course name from designated department
+'''
+def getCourse(str school, str department, str courseName):
+    pass
+
+
 # Course information displayed to users & can be editted by user in very beginning
 class CourseInfo:
 
-    # Set course name & store it into database.
-    # Manipulated by user.
-    def setName():
+    '''
+    Init CourseInfo.
+    If course doesn't exist in database, create this course
+    '''
+    def __init__(self, str school, str department, str courseName):
+        pass
+    
+    '''
+    Set course name to newName & store it into database
+    Manipulated by user
+    '''
+    def setName(self, str newName):
         pass
 
-    # Get course name from database
-    def getName():
+    '''
+    Set description & store it into database
+    Manipulated by user
+    '''
+    def setDescription(self, str description):
         pass
 
-    # Set description & store it into database
-    # Manipulated by user
-    def setDescription():
+    '''
+    Set website & store into database
+    Manipulated by user
+    '''
+    def setWebsite(self, str website):
         pass
 
-    # Get course description from database
-    def getDescription():
+    '''
+    Set department that course belongs to & store it into database
+    Manipulated by user
+    '''
+    def setDepartment(self, str department):
         pass
 
-    # Set website & store into database
-    # Manipulated by user
-    def setWebsite():
+    '''
+    Set school that course belongs to & store it into database
+    Manipulated by user
+    '''
+    def setSchool(self, str school):
         pass
 
-    # Get website from database
-    def getWebsite():
-        pass
 
-    # Set department that course belongs to & store it into database
-    # Manipulated by user
-    def setDepartment():
-        pass
-
-    # Get department that course belongs to
-    def getDepartment():
-        pass
-
-    # Set school that course belongs to & store it into database
-    # Manipulated by user
-    def setSchool():
-        pass
-
-    # Get school that course belongs to
-    def getSchool():
-        pass
-
+'''
+Get UserInfo via username
+'''
+def getUser(str username):
+    pass
 
 # User's profile that can be obtained or modified
 class UserInfo:
-
-    # Set school that user attends & modify it in database
-    # Manipulated by user
-    def setSchool():
+    '''
+    Init UserInfo.
+    If user doesn't exist in database, create this course
+    '''
+    def __init__(self, str username, str school, str department):
+        pass
+    
+    '''
+    Set school that user attends & modify it in database
+    Manipulated by user
+    '''
+    def setSchool(self, str school):
         pass
 
-    # Get school that user attends
-    def getSchool():
+    '''
+    Set department that user attends & modify it in database
+    Manipulated by user
+    '''
+    def setDepartment(self, str department):
         pass
 
-    # Set department that user attends & modify it in database
-    # Manipulated by user
-    def setDepartment():
+    '''
+    Set grade that user is in
+    Manipulated by user
+    '''
+    def setGrade(self, str grade):
         pass
 
-    # Get department that user attends
-    def getDepartment():
-        pass
 
-    # Set grade that user is in
-    # Manipulated by user
-    def setGrade():
-        pass
-
-    # Get grade that user is in
-    def getGrade():
-        pass
-
+'''
+Get OverallRate via CourseInfo which is already set up with school, department & course name
+'''
+def getOverallRate(str CourseInfo):
+    pass
 
 # Overall rate displayed to users
 class OverallRate:
 
-    # Get rate table from database
-    def getRateTable():
+    '''
+    rateTable[] contains all sectioned rates from all rated users, with types of whatever back-end like
+    totalRate is the overall rate relying on scores in rateTable[], with type of whatever back-end like
+    '''
+    def __init__(self, dict rateTable, str totalRate):
         pass
 
-    # Get total rate from sectioned rates
-    def getTotalRate():
-        pass
 
+'''
+Get an array of detailed rates for one course, with type of DetailedRate
+'''
+def getDetailedRateList(str course):
+    pass
 
-# One detailed rate displayed to users
+# Detailed rate raised by one user & displayed to other users
 class DetailedRate:
 
-    # Get the time that rate was raised
-    def getRateTime():
-        pass
+    '''
+    sectionedRates[] is user's rates in different section in rating criteria
+    totalRate is the total rate relying on scores in sectionedRates[]
+    comment is what user said about this course
+    recordTime is when he rated
+    semester refers to the semester he attended this course
+    tags[] is a group of tags easy for other users to see the main idea of this rate,
+    set up by the rating user, contains tags fewer than 4
+    teacher is teacher
+    countOfLikes is how many likes this rate get
+    countOfDislikes is how many dislikes this rate get
+    '''
+    def __init__(self, str sectoinedRates[], str totalRate, str comment,
+                 str recordTime, str semester, str tags[],
+                 str teacher, int countOfLikes, int countOfDislikes):
 
-    # Get the semester the user gives rate was in
-    def getSemester():
-        pass
-
-    # Get the tags the user provides
-    def getTags():
-        pass
-
-    # Get the teacher teaching the course that the user attends
-    def getTeacher():
-        pass
-
-    # Get the user's comment about this course
-    def getComment():
-        pass
-
-    # Get the sectioned rates the user provides
-    def getSectionedRates():
-        pass
-
-    # Get the total rate from the calculation of sectioned rates
-    def getTotalRate():
-        pass
-
-    # Get the count of user that like this rate
-    def getLikesCount():
-        pass
-
-    # Get the count of user that dislike this rate
-    def getDislikesCount():
-        pass
-
-    # Get the discussion under this rate
-    def getDiscussion():
-        pass
