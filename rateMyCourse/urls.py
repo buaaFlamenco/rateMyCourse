7aefc504f383e3cea9462fee4b33b4dccba9f41f
Donemeb <tyseven7@163.com>
2017-10-26 21:17:15 +0800

    readd rateMyCourse/urls.py

--- rateMyCourse/urls.py
+++ rateMyCourse/urls.py
@@ -3,5 +3,15 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'', views.index),
+    #GET
+    url(r'^$', views.getIndex, name='getIndex'),
+    url(r'^index/$', views.getIndex, name='getIndex'),
+    url(r'^search/$', views.searchSchool, name='searchSchool'),
+    url(r'^course/(?P<course_id>[0-9A-Z]+)/$', views.getCourse, name='getCourse'),
+
+    #POST
+    url(r'^signIp/$', views.signIn, name='signIn'),
+    url(r'^signUp/$', views.signUp, name='signUp'),
+    url(r'^course_addComment/$', views.courseAddComment, name='courseAddComment'),
+    url(r'^course_addRate/$', views.courseAddRate, name='courseAddRate'),
 ]
