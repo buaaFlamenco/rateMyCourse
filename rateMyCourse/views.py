9e6263f1092bc2347b6626cf165c0b07481f80b9
123 <123@123.123>
2017-10-26 20:17:51 +0800

    show index.html succesful

--- rateMyCourse/views.py
+++ rateMyCourse/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+def index(request):
+    return render(request, "rateMyCourse/index.html")
+
 #GET
 def searchSchool(request):
     school = request.GET.get('school');
