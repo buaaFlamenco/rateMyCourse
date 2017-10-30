058aca06d63c13745fe8a78684db4da51c83bec9
Hesitater <yizimu@gmail.com>
2017-10-30 19:39:13 +0800

    Get department

--- rateMyCourse/static/ratemycourse/javascript/index.js
+++ rateMyCourse/static/ratemycourse/javascript/index.js
@@ -1,11 +1,25 @@
 $(document).ready(function(){
-  $.ajax('/getSchool', {dataType: 'json'}).done(function(data){
+  $.ajax('/getSchool', {dataType:'json'}).done(function(data){
     var schoolList = $("#schoolList")
     for (var i = 0; i < data.school.length; i++) {
-      schoolList.append("<a class='dropdown-item btn btn-primary'>" + data.school[i] + "</a>")
+      schoolList.append("<a class='dropdown-item btn btn-primary school'>" + data.school[i] + "</a>")
     }
-  $(".dropdown-item").click(function(){
-    $(this).parent().prev().text($(this).text());
-  })
+    $(".dropdown-item.school").click(function(){
+      $(this).parent().prev().text($(this).text())
+      $.ajax('/getDepartment',{
+        dataType:'jason',
+        data:{'school':$(this).text()}
+      }).done(function(){
+        var departmentList = $("#departmentList")
+        departmentList.children().remove()
+        for (var i = 0; i < data.department.length; i++) {
+          departmentList.append("<a class='dropdown-item btn btn-primary department'>" +
+           data.department[i] + "</a>")
+        }
+        $(".dropdown-item.department").click(function(){
+          $(this).parent().prev().text($(this).text())
+        })
+      })
+    })
   })
 })
