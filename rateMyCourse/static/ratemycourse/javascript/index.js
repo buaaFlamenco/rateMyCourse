838c4dcdf21327050414f7aa355124e3ebb45359
123 <123@123.123>
2017-10-30 19:19:25 +0800

    fix bug in getting schoolList

--- rateMyCourse/static/ratemycourse/javascript/index.js
+++ rateMyCourse/static/ratemycourse/javascript/index.js
@@ -1,15 +1,11 @@
 $(document).ready(function(){
-  // alert("!!!!!!!!!")
   $.ajax('/getSchool', {dataType: 'json'}).done(function(data){
     var schoolList = $("#schoolList")
-    alert("!!!!!!!!!")
     for (var i = 0; i < data.school.length; i++) {
       schoolList.append("<a class='dropdown-item btn btn-primary'>" + data.school[i] + "</a>")
     }
-  }).fail(function(xhr, status){
-    alert(xhr.status + "   "+status)
-  })
   $(".dropdown-item").click(function(){
     $(this).parent().prev().text($(this).text());
+  })
   })
 })
