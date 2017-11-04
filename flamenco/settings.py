af61a58e7eac672d1cadff7036b004de77ad20dc
123 <123@123.123>
2017-11-05 00:55:03 +0800

    cmt

--- flamenco/settings.py
+++ flamenco/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,10 +77,18 @@ WSGI_APPLICATION = 'flamenco.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ratemycourse',
+        'USER': 'root',
+        'PASSWORD': 'flamenco',
+        'HOST': '127.0.0.1',
     }
+
 }
 
 
