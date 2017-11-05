76a6834a67f7cbbb5605925ea4bcda7ccb58fa5e
yang <914081693@qq.com>
2017-11-05 16:21:10 +0800

    重构了课程页面框架

--- rateMyCourse/static/ratemycourse/javascript/coursePage.js
+++ rateMyCourse/static/ratemycourse/javascript/coursePage.js
@@ -1,166 +1,185 @@
 'use strict'
 
-var sNum = 3;
-
-function setScores(scores) {
-    var tables = document.getElementsByClassName("rating");
-    for (var i = 0; i < sNum; i++) {
-        var lis = tables[i].getElementsByTagName("i");
-        for (var j = 0; j < scores[i]; j++) {
-            lis[j].setAttribute("class", "fa fa-star");
-        }
-    }
-}
-
-function generateGrid(imageUrls, text, headline) {
-        var ScreenGridHtml = `
-            <tr>
-                <td><img></td>
-                <td>
-                    <table>
-                        <tr><h1></tr>
-                        <tr><p></tr>
-                        <tr>
-                            <div>
-                                <img>
-                                <img>
-                                <img>
-                            </div>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
+function generateGrid(imageUrls,userName, iTerm, iTeacher, iToal, text, time) {
+    var ScreenGridHtml = `
+        <div>
+            <img>
+            <p>
+        </div>
+            <table>
+                <tr>
+                    <td><p></td>
+                    <td><p></td>
+                    <td><p></td>
+                </tr>
+                <tr>
+                    <td><p></td>
+                    <td><p></td>
+                    <td><p></td>
+                </tr>
+            <table>
+        <div>
+            <p>
+        </div>
+        <div>
+            <p>
+        </div>
+        <br/>
+        <br/>
+        <br/>
         `;
+        
         // create div
-        var commentGrid = document.createElement("table");
+        var commentGrid = document.createElement("div");
         commentGrid.id = "commentGrid";
         commentGrid.innerHTML = ScreenGridHtml;
-        //insert user image
+        //insert user image and name
         var imageTag = commentGrid.getElementsByTagName("img");
         imageTag[0].src = imageUrls;
-        imageTag[0].width = "150";
-        imageTag[0].setAttribute("class", "img-fluid d-block mb-5 m-5");
-        // insert headLine
-        var headlineTag = commentGrid.getElementsByTagName("h1");
-        var node1 = document.createTextNode(headline);
-        headlineTag[0].appendChild(node1);
-        headlineTag[0].setAttribute("class", "display-5 mb-4 text-dark");
+        imageTag[0].width = "86";
+        imageTag[0].setAttribute("class", "img-thumbnail");
+        imageTag[0].setAttribute("style", "margin-bottom:16px;margin-top:16px");
+
+        var pTags = commentGrid.getElementsByTagName("p");
+        var userNameNode = document.createTextNode(userName);
+        pTags[0].appendChild(userNameNode);
+        pTags[0].setAttribute("class", "userName");
+  
+        // insert information
+        var term = document.createTextNode("开课学期");
+        var teacher = document.createTextNode("授课教师");
+        var total = document.createTextNode("总评");
+        var vTerm = document.createTextNode(iTerm);
+        var vTeacher = document.createTextNode(iTeacher);
+        var vTotal = document.createTextNode(iToal);
+        pTags[1].appendChild(term);
+        pTags[2].appendChild(teacher);
+        pTags[3].appendChild(total);
+        pTags[4].appendChild(vTerm);
+        pTags[5].appendChild(vTeacher);
+        pTags[6].appendChild(vTotal);
+
         //insert text
-        var textTag = commentGrid.getElementsByTagName("p");
-        var node = document.createTextNode(text);
-        textTag[0].appendChild(node);
-        textTag[0].setAttribute("class", "lead mb-5 text-dark");
-        // insert icons
-        imageTag[1].src = "../../static/ratemycourse/images/store.png";
-        imageTag[2].src = "../../static/ratemycourse/images/feedback.png";
-        imageTag[3].src = "../../static/ratemycourse/images/more.png";
+        var textnode = document.createTextNode(text);
+        pTags[7].appendChild(textnode);
+        pTags[7].setAttribute("style", "margin-top:16px;text-align:left; width:70%")
+        //inset time
+        var timenode = document.createTextNode(time);
+        pTags[8].appendChild(timenode);
+        pTags[8].setAttribute("style", "width:100%;text-align:right;margin-top:32px")
 
-        var trTags = commentGrid.getElementsByTagName("tr");
-        trTags[0].setAttribute("class", "commentTr");
-        trTags[0].setAttribute("style", "background:#FFF; color:#FFF");
-
-        var tdTags = commentGrid.getElementsByTagName("div");
-       // tdTags[2].align = "right";
-        //tdTags[3].align = "right";
-    //tdTags[4].align = "right";
-        tdTags[0].setAttribute("style", "float:right");
-
+        //css
+        var divTags = commentGrid.getElementsByTagName("div");
+        divTags[0].setAttribute("class", "row text-center");
+        divTags[1].setAttribute("class", "row text-center");
+        divTags[2].setAttribute("class", "row");
+        divTags[0].setAttribute("style", "width:70%;border-bottom:1px #e4e4e4 solid;");
+        divTags[2].setAttribute("style", "width:100%;border-bottom:1px #e4e4e4 solid;");
+        var tableTag = commentGrid.getElementsByTagName("table");
+        tableTag[0].setAttribute("style", "width:70%; margin-top:16px;border-bottom:1px #e4e4e4 solid");
         return commentGrid;
 }
 
-function setComments(){//get comments list from service
-	$.ajax('/getComment', {
-		dataType: 'json',
-		data: {'course_number': $('#courseNumber').text()},
-	}).done(function(data){
-    	var imgurl = "../../static/ratemycourse/images/user.png";
-    	var headLine = "Image and gradient intro";
-    	var text = "Get a fluid web page working on all devices with the Bootstrap 4 grid system.";
-    	var parents = document.getElementById("commentDiv");
-    	var comment = document.getElementById("commentGrid");
-    	if (comment) {
-    	    parents.removeChild(comment);
-    	}
-    	for(var i = 0; i < data.comments.length; i++){
-    	    //generate a new row
-    	    var Grid=generateGrid(imgurl, data.comments[i].content, data.comments[i].username);
-    	    //insert this new row
-    	    parents.appendChild(Grid);
-    	    var br = document.createElement("br");
-    	    parents.appendChild(br);
-    	}
-	})
+function setComments() {//get comments list from service
+    $.ajax('/getComment', {
+        dataType: 'json',
+        data: {'course_number': $('#courseNumber').text()},
+    }).done(function(data){
+        var imgurl = "imgs/user.png";
+        var text = "Get a fluid web page working on all devices with the Bootstrap 4 grid system.Get a fluid web page working on all devices with the Bootstrap 4 grid system.Get a fluid web page working on all devices with the Bootstrap 4 grid system.";
+        var userName = "unkown";
+        var iTerm = "2017 秋季";
+        var iTeacher = "罗杰";
+        var iToal = "5.0";
+        var time = "2017/11/13";
+        var parents = document.getElementById("commentDiv");
+        var comment = document.getElementById("commentGrid");
+        if (comment) {
+            parents.removeChild(comment);
+        }
+        for(var i=0; i<10; i++){
+            //generate a new row
+            var Grid = generateGrid(imgurl, userName, iTerm, iTeacher, iToal, text, time);
+            //insert this new row
+            parents.appendChild(Grid);
+        }
+    })
 }
+//var imgurl = {{userimg_list|safe}};
+//var userName = {{userName_list|safe}};
+//var iTerm = {{term_list|safe}};
+//var iTeacher = {{teacher_list|safe}};
+//var iTotal = {{total_list|safe}};
+//var text = {{text_list|safe}};
+//var time = {{time_list|safe}};
 
-
-$(document).ready(function() {
-  // Login widget set according to cookie
-  if($.cookie('username') == undefined) {
-    $("#menuUser").hide()
-    $("#menuLogin").show()
-  }
-  else{
-    $("#menuLogin").hide()
-    $("#menuUser").show()
-    $("#navUser").text($.cookie('username'))
-  }
-	//var scores = {{score_list|safe}};
-	$.ajax('/getOverAllRate', {dataType: 'json', data: {'course_number': $('#courseNumber').text()}, }).done(function(data){
-		setScores(data.rate)
-	})
-	//var userimg = {{userimg_list}};
-	//var text = {{text_list}};
-	//var headLine = {{headLine_list}};
-	setComments();
-	document.getElementById("toComment").onclick = function () { console.log("cliked");; }// turn to the page of grading
+$(document).ready(function () {
+    // Login widget set according to cookie
+    if ($.cookie('username') == undefined) {
+        $("#menuUser").hide()
+        $("#menuLogin").show()
+    }
+    else {
+        $("#menuLogin").hide()
+        $("#menuUser").show()
+        $("#navUser").text($.cookie('username'))
+    }
+    $.ajax('/getOverAllRate', { dataType: 'json', data: { 'course_number': $('#courseNumber').text() }, }).done(function (data) {
+        setScores(data.rate)
+    })
+    setComments();
+    document.getElementById("toComment").onclick = function () {
+        console.log("cliked");
+        // window.location.href = "http://blog.sina.com.cn/mleavs";
+    }// turn to the page of grading
 })
 
 function Func_signUp() {
-  $.ajax("/signUp/", {
-    dataType: 'json',
-    type: 'POST',
-    data: {
-      "username": $("#inputUsername").val(),
-      "mail": $("#inputEmail").val(),
-      "password": $("#inputPassword").val(),
-    }
-  }).done(function(data) {
-    if (data.statCode != 0) {
-      alert(data.errormessage)
-    } else {
-      $("#menuLogin").hide()
-      $("#menuUser").show()
-      $("#navUser").text(data.username)
-      $.cookie('username', data.username)
+    $.ajax("/signUp/", {
+        dataType: 'json',
+        type: 'POST',
+        data: {
+            "username": $("#inputUsername").val(),
+            "mail": $("#inputEmail").val(),
+            "password": $("#inputPassword").val(),
+        }
+    }).done(function (data) {
+        if (data.statCode != 0) {
+            alert(data.errormessage)
+        } else {
+            $("#menuLogin").hide()
+            $("#menuUser").show()
+            $("#navUser").text(data.username)
+            $.cookie('username', data.username)
 
-    }
-  })
-  return false
+        }
+    })
+    return false
 }
 
 function Func_signIn() {
-  $.ajax("/signIn/", {
-    dataType: 'json',
-    type: 'POST',
-    data: {
-      "username": $("#username").val(),
-      "password": $("#password").val()
-    }
-  }).done(function(data) {
-    if(data.statCode != 0) {
-      alert(data.errormessage)
-    } else {
-      $("#menuLogin").hide()
-      $("#menuUser").show()
-      $("#navUser").text(data.username)
-      $.cookie('username', data.username)
-    }
-  })
-  return false
+    $.ajax("/signIn/", {
+        dataType: 'json',
+        type: 'POST',
+        data: {
+            "username": $("#username").val(),
+            "password": $("#password").val()
+        }
+    }).done(function (data) {
+        if (data.statCode != 0) {
+            alert(data.errormessage)
+        } else {
+            $("#menuLogin").hide()
+            $("#menuUser").show()
+            $("#navUser").text(data.username)
+            $.cookie('username', data.username)
+        }
+    })
+    return false
 }
 
 function Func_signOut() {
-  $("#menuUser").hide()
-  $("#menuLogin").show()
-  return false
+    $("#menuUser").hide()
+    $("#menuLogin").show()
+    return false
 }
