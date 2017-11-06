1c621c447637fe92b00bce155cb313c7ffbf60b9
123 <123@123.123>
2017-11-06 12:40:51 +0800

    search engine into site

--- flamenco/settings.py
+++ flamenco/settings.py
@@ -13,10 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import pymysql
 pymysql.install_as_MySQLdb()
-<<<<<<< HEAD
 
-=======
->>>>>>> de509fec521eac5b41425ea44c364ab8eafdf91f
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
