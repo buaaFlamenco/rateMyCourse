dd4cfca3f8ab17eeeade0d2388eb75c31ec6c12f
Hesitater <yizimu@gmail.com>
2017-12-02 23:43:33 +0800

    User page hover & click feature added

--- rateMyCourse/static/ratemycourse/javascript/userPage.js
+++ rateMyCourse/static/ratemycourse/javascript/userPage.js
@@ -1,4 +1,55 @@
 $(document).ready(function() {
+  // Form validation for Sign in / Sign up forms
   validateSignUp()
   validateSignIn()
+
+  // Evaluations section is open on default
+  viewEvaluations();
+
+  // Option bar settings
+  $("#choiceDiscussions").hover(
+    optionMoveRight, optionMoveLeft
+  ).click(function(){
+    $("#choiceDiscussions").unbind("mouseleave")
+    $("#choiceEvaluations").on("mouseleave", optionMoveRight)
+    optionMoveRight()
+    viewDiscussions()
+  })
+
+  $("#choiceEvaluations").hover(
+    optionMoveLeft, optionMoveRight
+  ).click(function(){
+    $("#choiceEvaluations").unbind("mouseleave")
+    $("#choiceDiscussions").on("mouseleave", optionMoveLeft)
+    optionMoveLeft()
+    viewEvaluations()
+  })
 })
+
+function optionMoveLeft(){
+  $("#optionBar").stop().animate({
+    marginLeft:"0px"
+  })
+}
+
+function optionMoveRight(){
+  $("#optionBar").stop().animate({
+    marginLeft:"70px"
+  })
+}
+
+function viewEvaluations(){
+  $("#choiceEvaluations").addClass("active");
+  $("#choiceDiscussions").removeClass("active");
+
+  $("#evaluations").show()
+  $("#discussions").hide()
+}
+
+function viewDiscussions(){
+  $("#choiceDiscussions").addClass("active");
+  $("#choiceEvaluations").removeClass("active");
+
+  $("#discussions").show()
+  $("#evaluations").hide()
+}
